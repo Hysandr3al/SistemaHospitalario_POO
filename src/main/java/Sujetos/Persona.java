74329cbbc64effaa2e8468b50b4d704f48c1b341
Andre <alerma75320@gmail.com>
2025-11-26 12:21:44 -0500

    Añado getters y setters a la clase persona

--- src/main/java/Sujetos/Persona.java
+++ src/main/java/Sujetos/Persona.java
@@ -8,8 +8,6 @@ public abstract class Persona {
     protected String sexo;
     protected int edad;
 
-
-
     public Persona(String nombres, String apellidos, String telefono, String dni, String sexo, int edad) {
         this.nombres = nombres;
         this.apellidos = apellidos;
@@ -19,6 +17,44 @@ public abstract class Persona {
         this.edad = edad;
     }
 
+    
+    //getters y setters
+    public String getNombres() {
+        return nombres;
+    }
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
     public abstract void mostrarInfo();
 }
