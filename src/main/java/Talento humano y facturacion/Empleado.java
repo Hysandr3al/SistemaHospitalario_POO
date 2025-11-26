3fe43ae4d9051aecdad1d71c81ce2a0eaf569dac
anthonyxD324 <114789553+anthonyxD324@users.noreply.github.com>
2025-11-26 13:07:57 -0500

    Update Empleado.java

--- src/main/java/Talento humano y facturacion/Empleado.java
+++ src/main/java/Talento humano y facturacion/Empleado.java
@@ -6,7 +6,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package TalentoHumano;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitaltalentofinanzas;
 
 import Sujetos.Persona;
 import java.util.Date;
