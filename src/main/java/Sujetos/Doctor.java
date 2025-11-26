c630bfd0f6fa65fe569418ea2c448bb489776d5b
Andre <alerma75320@gmail.com>
2025-11-25 19:51:22 -0500

    Añado clases base: Persona, Paciente, Doctor

---
+++ src/main/java/Sujetos/Doctor.java
@@ -0,0 +1,30 @@
+package Sujetos;
+
+public class Doctor extends Persona{
+    private String especialidad;
+    private String horario;
+    private double salario;
+
+
+
+    public Doctor(String nombres, String apellidos, String telefono, String dni, String sexo, int edad,
+                  String especialidad, String horario, double salario) {
+        super(nombres, apellidos, telefono, dni, sexo, edad);
+        this.especialidad = especialidad;
+        this.horario = horario;
+        this.salario = salario;
+    }
+
+    public RecetaMedica crearRecetaMedica(Paciente paciente){
+        return
+    }
+
+    public String getEspecialidad(){
+        return especialidad;
+    }
+
+    @Override
+    public void mostrarInfo(){
+        System.out.println("Doctor: " + nombres + " " + apellidos + " - Especialidad: " + especialidad);
+    };
+}
