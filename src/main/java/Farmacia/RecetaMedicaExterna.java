f6e21ac9c993231be97ba4318d067d16063acfef
Andre <alerma75320@gmail.com>
2025-11-30 13:05:57 -0500

    Añado el id para las dos clases de recetaMedica, ademas de sus getters y setters

--- src/main/java/Farmacia/RecetaMedicaExterna.java
+++ src/main/java/Farmacia/RecetaMedicaExterna.java
@@ -7,6 +7,7 @@ import Receta.RecetaMedica;
 
 public class RecetaMedicaExterna extends RecetaMedica{
     private String nombreCliente;
+    private int idRecetaExterna;
 
     public RecetaMedicaExterna(List<DetalleMedicamento> detalleMedicamentos, String nombreCliente) {
         super(detalleMedicamentos);
@@ -16,5 +17,17 @@ public class RecetaMedicaExterna extends RecetaMedica{
     public String getNombreCliente(){
         return nombreCliente;
     }
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getIdRecetaExterna() {
+        return idRecetaExterna;
+    }
+    public void setIdRecetaExterna(int idRecetaExterna) {
+        this.idRecetaExterna = idRecetaExterna;
+    }
+    
+
 }
 
--- src/main/java/Receta/RecetaMedicaHospital.java
+++ src/main/java/Receta/RecetaMedicaHospital.java
@@ -10,29 +10,42 @@ public class RecetaMedicaHospital extends RecetaMedica {
     private int idReceta;
     private Doctor doctor;
     private Paciente paciente;
+    private int idRecetaHospital;
+
     public RecetaMedicaHospital(List<DetalleMedicamento> detalleMedicamentos, int idReceta, Doctor doctor, Paciente paciente) {
         super(detalleMedicamentos);
         this.idReceta = idReceta;
         this.doctor = doctor;
         this.paciente = paciente;
     }
+    
     public int getIdReceta() {
         return idReceta;
     }
     public void setIdReceta(int idReceta) {
         this.idReceta = idReceta;
     }
+    
     public Doctor getDoctor() {
         return doctor;
     }
     public void setDoctor(Doctor doctor) {
         this.doctor = doctor;
     }
+    
     public Paciente getPaciente() {
         return paciente;
     }
     public void setPaciente(Paciente paciente) {
         this.paciente = paciente;
     }
+
+    public int getIdRecetaHospital() {
+        return idRecetaHospital;
+    }
+    public void setIdRecetaHospital(int idRecetaHospital) {
+        this.idRecetaHospital = idRecetaHospital;
+    }
+    
 
 }
