0ef6f80807de342825fc566079c691e098df7ee6
Andre <alerma75320@gmail.com>
2025-11-30 12:58:21 -0500

    Agrego idDetalle, tipoReceta y idReceta. y sus constructores

--- src/main/java/Receta/DetalleMedicamento.java
+++ src/main/java/Receta/DetalleMedicamento.java
@@ -4,6 +4,9 @@ import Farmacia.Medicamento;
 public class DetalleMedicamento {
     private int cantidad;
     private Medicamento medicamento;
+    private int idDetalle;
+    private char tipoReceta; //H o E
+    private int idReceta;
 
     public DetalleMedicamento(Medicamento medicamento, int cantidad){
         this.medicamento = medicamento;
@@ -17,7 +20,30 @@ public class DetalleMedicamento {
     public int getCantidad(){
         return cantidad;
     }
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
 
+    public int getIdDetalle(){
+        return idDetalle;
+    }
+    public void setIdDetalle(int idDetalle){
+        this.idDetalle = idDetalle;
+    }
+
+    public char getTipoReceta(){
+        return tipoReceta;
+    }
+    public void setTipoReceta(char tipoReceta){
+        this.tipoReceta= tipoReceta;
+    }
+
+    public int getIdReceta(){
+        return idReceta;
+    }
+    public void setIdReceta(int idReceta){
+        this.idReceta = idReceta;
+    }
 
     public double calcularSubTotal(){
         return medicamento.getPrecio() * cantidad;
