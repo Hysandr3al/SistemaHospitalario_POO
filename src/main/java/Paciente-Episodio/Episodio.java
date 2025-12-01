c6400608b5c4a54202e267fe67529276d06c6eb3
Mathieu <matcaballero03@gmail.com>
2025-11-30 19:11:08 -0500

    Actualizada clase Episodio añadierndo getters

--- src/main/java/Paciente-Episodio/Episodio.java
+++ src/main/java/Paciente-Episodio/Episodio.java
@@ -3,12 +3,30 @@ package hospital.pacienteepisodio;
 import java.time.LocalDate;
 
 public abstract class Episodio {
+
+    protected int idEpisodio;     // ← NECESARIO PARA EL DAO
     protected String descripcion;
     protected LocalDate fecha;
 
     public Episodio(String descripcion) {
         this.descripcion = descripcion;
         this.fecha = LocalDate.now();
+    }
+
+    public int getIdEpisodio() {
+        return idEpisodio;
+    }
+
+    public void setIdEpisodio(int id) {   // ← El DAO lo colocará
+        this.idEpisodio = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
     }
 
     @Override
